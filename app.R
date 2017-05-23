c040867237c2393ac4a69a84d10a870c9f2b2de7
Mike Hollander <hollander@gmail.com>
2017-05-22 22:12:54 -0400

    Complete refactoring of code to allow for other races.  Added controller race and several judge races.

--- app.R
+++ app.R
@@ -10,7 +10,7 @@ library(DT)
 source("helper.R")
 
 # Define UI for application that draws a histogram
-ui <- navbarPage("2017 Philly DA Primary Vote Explorer", id="nav", 
+ui <- navbarPage("2017 Philly Primary Vote Explorer", id="nav", 
                  
       tabPanel("Interactive Map",
         div(class="outer",
@@ -20,6 +20,13 @@ ui <- navbarPage("2017 Philly DA Primary Vote Explorer", id="nav",
                           draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
                           width = 330, height = "auto",
                           
+                          selectInput("office", label = h4("Office"),
+                                      choices = c("District Attorney" = "DA", 
+                                                  "Controller" = "Controller",
+                                                  "CCP Judge (D)" = "CCP",
+                                                  "Commonwealth Ct (D)" = "CWCD",
+                                                  "Commonwealth Ct (R)" = "CWCR"),
+                                      selected = "DA"),
                           selectInput("candidate", label = h4("Candidate"),
                                         choices = names(DApercents[,c(-1,-2,-3)]), 
                                         selected = "KRASNER..L")
@@ -31,7 +38,7 @@ ui <- navbarPage("2017 Philly DA Primary Vote Explorer", id="nav",
       ),
       
       tabPanel("Data Explorer", 
-        h2("Primary Results, by Ward and Division"),
+        htmlOutput("DTTitle",container= tags$h2),
         dataTableOutput("voteTable"),
         tags$div(id="cite",
                  'Created by ', tags$a(href="mailto:hollander@gmail.com", "Michael Hollander"), "Available on ", tags$a(href="https://github.com/mhollander/2017PhillyDAPrimary","GitHub")
@@ -77,41 +84,147 @@ ui <- navbarPage("2017 Philly DA Primary Vote Explorer", id="nav",
       
   )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  session$userData$DA <- "KRASNER..L"
+  session$userData$CCP <- "KRISTIANSSON..V"
+  session$userData$Controller <- "RHYNHART..R"
+  session$userData$CWCD <- "CEISLER..E"
+  session$userData$CWCR <- "LALLEY..P"
+  session$userData$Office <- "DA"
   
   output$voteMap <- renderLeaflet({
     return(daVoteMap)
   })
   
+  output$DTTitle <- renderUI({
+    
+    HTML(paste(input$office,"Primary Results, by Ward and Division"))
+
+  })
   
-  output$voteTable <- renderDataTable({
-    daVoteTable <- DT::datatable(DApercents[,-1],
+    output$voteTable <- renderDataTable({
+    
+      
+    outputTable <- switch(input$office,
+           "DA" = DApercents,
+           "CCP" = CCPpercents,
+           "Controller" = Contpercents,
+           "CWCD" = CWDpercents,
+           "CWCR" = CWRpercents)
+    
+    aVoteTable <- DT::datatable(outputTable[,-1],
                                 options=list(
-                                  pageLength = 30,
-                                  lengthMenu = list(c(15, 30, 60, -1),c("15", "30", "60", 'All')),
+                                  pageLength = 10,
+                                  lengthMenu = list(c(10, 30, 60, -1),c("10", "30", "60", 'All')),
                                   order = list(0,'asc'),
                                   searching=TRUE
                                 ), 
                                 class="stripe",
                                 rownames=FALSE
     ) 
-    return(daVoteTable)
+    return(aVoteTable)
   })
   
    observe({
-    columnData = precincts@data[[input$candidate]]
-    
-    # otherLayer = ifelse(input$vulnerable=="vulnerableLowStateMargin","vulnerableHighPresMargin","vulnerableLowStateMargin")
-    leafletProxy("voteMap", data=precincts) %>%
-      # hideGroup(otherLayer) %>%
-      # showGroup(input$vulnerable)
-      clearShapes() %>%
-      addPolygons(fillColor = ~pal(columnData),
-                  fillOpacity = 0.8,
-                  color="#BDBDC3",
-                  weight = 1,
-                  popup = votePopup)
-  })
+     #if (is.null(session$userData$CCP) || is.na(session$userData$CCP) || session$userData$CCP=="")
+    #   session$userData$CCP <- "KRISTIANSSON..V"
+     
+     office <- input$office
+     session$userData$office <- office                     
+     
+     cNames <- switch(office,
+                      "DA" = names(DApercents[,c(-1,-2,-3)]),
+                      "CCP" = names(CCPpercents[,c(-1,-2,-3)]),
+                      "Controller" = names(Contpercents[,c(-1,-2,-3)]),
+                      "CWCD" = names(CWDpercents[,c(-1,-2,-3)]),
+                      "CWCR" = names(CWRpercents[,c(-1,-2,-3)]))
+     
+     cDefault <- switch(office,
+                        "DA" = session$userData$DA,
+                        "CCP" = session$userData$CCP,
+                        "Controller" = session$userData$Controller,
+                        "CWCD" = session$userData$CWCD,
+                        "CWCR" = session$userData$CWCR)
+     
+     updateSelectInput(session, "candidate", 
+                        choices = cNames, 
+                        selected = cDefault)
+      
+   })
+   
+   observe({
+     if (session$userData$office == "DA") {
+       
+       columnData = precincts@data[[input$candidate]]
+       session$userData$DA <- input$candidate
+       
+        # otherLayer = ifelse(input$vulnerable=="vulnerableLowStateMargin","vulnerableHighPresMargin","vulnerableLowStateMargin")
+        leafletProxy("voteMap", data=precincts) %>%
+          clearShapes() %>%
+          addPolygons(fillColor = ~pal(columnData),
+                    fillOpacity = 0.8,
+                    color="#BDBDC3",
+                    weight = 1,
+                    popup = votePopup)
+      
+    }
+   
+   
+    else if (session$userData$office == "CCP") {
+     columnData = precinctsCCP@data[[input$candidate]]
+     session$userData$CCP <- input$candidate
+
+      # otherLayer = ifelse(input$vulnerable=="vulnerableLowStateMargin","vulnerableHighPresMargin","vulnerableLowStateMargin")
+     leafletProxy("voteMap", data=precinctsCCP) %>%
+       clearShapes() %>%
+       addPolygons(fillColor = ~ccppal(columnData),
+                   fillOpacity = 0.8,
+                   color="#BDBDC3",
+                   weight = 1,
+                   popup = votePopupCCP)
+    }
+     
+     else if (session$userData$office == "Controller") {
+       columnData = precinctsCont@data[[input$candidate]]
+       session$userData$Controller <- input$candidate
+       
+       # otherLayer = ifelse(input$vulnerable=="vulnerableLowStateMargin","vulnerableHighPresMargin","vulnerableLowStateMargin")
+       leafletProxy("voteMap", data=precinctsCont) %>%
+         clearShapes() %>%
+         addPolygons(fillColor = ~pal(columnData),
+                     fillOpacity = 0.8,
+                     color="#BDBDC3",
+                     weight = 1,
+                     popup = votePopupCont)
+     }
+
+     else if (session$userData$office == "CWCD") {
+       columnData = precinctsCWD@data[[input$candidate]]
+       session$userData$CWCD <- input$candidate
+       
+       leafletProxy("voteMap", data=precinctsCWD) %>%
+         clearShapes() %>%
+         addPolygons(fillColor = ~cwdpal(columnData),
+                     fillOpacity = 0.8,
+                     color="#BDBDC3",
+                     weight = 1,
+                     popup = votePopupCWD)
+     }
+     else if (session$userData$office == "CWCR") {
+       columnData = precinctsCWR@data[[input$candidate]]
+       session$userData$CWCR <- input$candidate
+       
+       leafletProxy("voteMap", data=precinctsCWR) %>%
+         clearShapes() %>%
+         addPolygons(fillColor = ~pal(columnData),
+                     fillOpacity = 0.8,
+                     color="#BDBDC3",
+                     weight = 1,
+                     popup = votePopupCWR)
+     }
+     
+   })
 }
 
 # Run the application 
--- helper.R
+++ helper.R
@@ -13,63 +13,118 @@ library(rmapshaper)
 library(rgdal)
 library(dplyr)
 
+
+getPopup <- function(votedata, mapdata, office) {
+  popup <-paste0("<strong>Ward:</strong> ", 
+                 mapdata$Ward, 
+                 "<br><strong>Division:</strong> ",
+                 mapdata$Division)
+  
+  for (candidate in sort(make.names(unique(votedata$Tape_Text[votedata$Office_Prop.Name==office])))) {
+    popup<- paste0(popup, "<br><strong>",candidate,":</strong> ", mapdata[[candidate]], "%")
+  }
+  
+  return(popup)
+}
+
+# makes a wide table with all of the candidates as columns, with precincts, etc.. and data cleaned up
+# also puts in vote totals for each precinct
+getOfficeVotes <- function(data,office) {
+  # create a table of DA votes by making a tall table wide based on precinct and DA race candidates
+  
+  officeVotes <-dcast(data[data$Office_Prop.Name == office,], Precinct_Name ~ Tape_Text, value.var="Vote_Count", fun.aggregate=sum)
+  # tidy up the names of the columns to remove spaces, commas, etc...
+  names(officeVotes)<- make.names(names(officeVotes))
+
+  # create a totals column
+  officeVotes$vote_totals <- rowSums(officeVotes[-1])
+
+  #Now split the precinct name into ward/precinct and then create a combined column since we'll need that for the mapping
+  officeVotes$Ward <- NULL
+  officeVotes$Division <- NULL
+  officeVotes[,c("Ward","Division")] <- str_split_fixed(as.character(officeVotes$Precinct_Name),"-",2)
+  officeVotes$Precinct_Name <- paste(officeVotes$Ward,officeVotes$Division, sep="")
+
+  return(officeVotes)
+}
+
+getOfficePercent <- function(data) {
+  #make a parallel table with percentages rather than absolute numbers of votes
+  #I bet there is a better wy to do this, without having to have a parallel table, but fuck it.
+  # for the second column to fourth to last column, divide by the third to last column, which is vote total and then make a percent
+  votePercents <- round(data[,c(2:(ncol(data)-3))]/data[,ncol(data)-2],3) * 100
+  votePercents$Precinct_Name <- data$Precinct_Name
+  votePercents$Division <- data$Division
+  votePercents$Ward <- data$Ward
+  
+  # reorder teh columns so that the precinct, ward, and division are up front; remove the totals column
+  votePercents <- votePercents[,c((ncol(votePercents)-2):ncol(votePercents),1:(ncol(votePercents)-3))]
+  return(votePercents)
+}
+
 # read the CSV and get rid of the strange last column
 votes <- read.csv("PRECINCT_2017517_H20_M51_S0.txt",sep="@")
 votes$X <- NULL
 # make the votes into integers
 votes$Vote_Count <- as.integer(votes$Vote_Count)
 
+# I want to be able to view the D and R DA Race together, so change the name of that prop
+levels(votes$Office_Prop.Name)[match("DISTRICT ATTORNEY-DEM",levels(votes$Office_Prop.Name))] <- "DISTRICT ATTORNEY-ALL"
+levels(votes$Office_Prop.Name)[match("DISTRICT ATTORNEY-REP",levels(votes$Office_Prop.Name))] <- "DISTRICT ATTORNEY-ALL"
 
-# create a table of DA votes by making a tall table wide based on precinct and DA race candidates
-DAvotes <- dcast(votes[votes$Office_Prop.Name %in% c("DISTRICT ATTORNEY-DEM", "DISTRICT ATTORNEY-REP"),], Precinct_Name ~ Tape_Text, value.var="Vote_Count", fun.aggregate=sum)
-
-# tidy up the names of the columns to remove spaces, commas, etc...
-names(DAvotes)<- make.names(names(DAvotes))
-
-# create a totals column
-DAvotes$vote_totals <- rowSums(DAvotes[-1])
-
-#Now split the precinct name into ward/precinct and then create a combined column since we'll need that for the mapping
-DAvotes$Ward <- NULL
-DAvotes$Division <- NULL
-DAvotes[,c("Ward","Division")] <- str_split_fixed(as.character(DAvotes$Precinct_Name),"-",2)
-DAvotes$Precinct_Name <- paste(DAvotes$Ward,DAvotes$Division, sep="")
+# create the vote tables with wide rather than long format and total columns
+DAvotes <- getOfficeVotes(votes, "DISTRICT ATTORNEY-ALL")
+CCPvotes <- getOfficeVotes(votes, "JUDGE OF THE COURT OF COMMON PLEAS-DEM")
+Contvotes <- getOfficeVotes(votes, "CITY CONTROLLER-DEM")
+CWDvotes <- getOfficeVotes(votes, "JUDGE OF THE COMMONWEALTH COURT-DEM")
+CWRvotes <- getOfficeVotes(votes, "JUDGE OF THE COMMONWEALTH COURT-REP")
 
 #make a parallel table with percentages rather than absolute numbers of votes
 #I bet there is a better wy to do this, without having to have a parallel table, but fuck it.
-DApercents <- round(DAvotes[,c(-1,-11,-12,-13)]/DAvotes[,11],3) * 100
-DApercents$Precinct_Name <- DAvotes$Precinct_Name
-DApercents$Division <- DAvotes$Division
-DApercents$Ward <- DAvotes$Ward
+DApercents <- getOfficePercent(DAvotes)
+CCPpercents <- getOfficePercent(CCPvotes)
+Contpercents <- getOfficePercent(Contvotes)
+CWDpercents <- getOfficePercent(CWDvotes)
+CWRpercents <- getOfficePercent(CWRvotes)
 
-DApercents <- DApercents[,c((ncol(DApercents)-2):ncol(DApercents),1:(ncol(DApercents)-3))]
-
-# map it!
+# first turn the shapefile into a usable object
 tmp <- tempdir()
 unzip("Political_Divisions.zip", exdir = tmp)
 precincts <- readOGR(dsn=tmp)
-precincts <- rmapshaper::ms_simplify(precincts)
+precincts <- rmapshaper::ms_simplify(precincts, keep=.04)
+
+precinctsCCP <- precincts
+precinctsCont <- precincts
+precinctsCWD <- precincts
+precinctsCWR <- precincts
 
 # use a left join b/c some districts have no data
 precincts@data = precincts@data %>%
   left_join(DApercents, by=c("DIVISION_N"="Precinct_Name"), copy=TRUE)
+precinctsCCP@data = precinctsCCP@data %>%
+  left_join(CCPpercents, by=c("DIVISION_N"="Precinct_Name"), copy=TRUE)
+precinctsCont@data = precinctsCont@data %>%
+  left_join(Contpercents, by=c("DIVISION_N"="Precinct_Name"), copy=TRUE)
+precinctsCWD@data = precinctsCWD@data %>%
+  left_join(CWDpercents, by=c("DIVISION_N"="Precinct_Name"), copy=TRUE)
+precinctsCWR@data = precinctsCWR@data %>%
+  left_join(CWRpercents, by=c("DIVISION_N"="Precinct_Name"), copy=TRUE)
+
+# now create our popups
+votePopup <- getPopup(votes, precincts, "DISTRICT ATTORNEY-ALL")
+votePopupCCP <- getPopup(votes,precinctsCCP,"JUDGE OF THE COURT OF COMMON PLEAS-DEM")
+votePopupCont <- getPopup(votes,precinctsCont,"CITY CONTROLLER-DEM")
+votePopupCWD <- getPopup(votes,precinctsCWD,"JUDGE OF THE COMMONWEALTH COURT-DEM")
+votePopupCWR <- getPopup(votes,precinctsCWR,"JUDGE OF THE COMMONWEALTH COURT-REP")
 
 
-votePopup <- paste0("<strong>Ward:</strong> ", 
-                      precincts$Ward, 
-                      "<br><strong>Division:</strong> ",
-                      precincts$Division,
-                      "<br><strong>Deni:</strong>", precincts$DENI..T, "%",
-                      "<br><strong>Shabazz:</strong>", precincts$EL.SHABAZZ..T, "%",
-                      "<br><strong>Grossman:</strong>", precincts$GROSSMAN..B, "%",
-                      "<br><strong>Khan:</strong>", precincts$KHAN..J, "%",
-                      "<br><strong>Krasner:</strong>", precincts$KRASNER..L, "%",
-                      "<br><strong>Negrin:</strong>", precincts$NEGRIN..R, "%",
-                      "<br><strong>O'Neill:</strong>", precincts$O.NEILL..J, "%",
-                      "<br><strong>Untermeyer:</strong>", precincts$UNTERMEYER..M, "%",
-                      "<br><strong>Write In:</strong>", precincts$Write_In, "%")
                     
 pal <- colorNumeric("YlOrBr",domain=0:100)
+# the max percent than any ccp D judge took in any district was 25%
+ccppal <- colorNumeric("YlOrBr",domain=0:25)
+
+# max percent for CWC D judge is 64%
+cwdpal <- colorNumeric("YlOrBr", domain=0:65)
 
 daVoteMap <- leaflet(data = precincts) %>%
   addProviderTiles("CartoDB.Positron") %>%
@@ -80,5 +135,23 @@ daVoteMap <- leaflet(data = precincts) %>%
               popup = votePopup) %>%
   setView(lng = -75.07, lat = 40, zoom=11)
 
-
+# 
+# ccpVoteMap <- leaflet(data = precinctsCCP) %>%
+#   addProviderTiles("CartoDB.Positron") %>%
+#   addPolygons(fillColor = ~ccppal(KRISTIANSSON..V), 
+#               fillOpacity = .65,
+#               color="#BDBDC3",
+#               weight = 1, 
+#               popup = votePopupCCP) %>%
+#   setView(lng = -75.07, lat = 40, zoom=11)
+# 
+# contVoteMap <- leaflet(data = precinctsCont) %>%
+#   addProviderTiles("CartoDB.Positron") %>%
+#   addPolygons(fillColor = ~ccppal(REINHART..R), 
+#               fillOpacity = .65,
+#               color="#BDBDC3",
+#               weight = 1, 
+#               popup = votePopup) %>%
+#   setView(lng = -75.07, lat = 40, zoom=11)
+# 
 
